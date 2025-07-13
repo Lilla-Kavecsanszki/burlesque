2de04689d2c193cf995b05c670a140222b7de92c
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2025-07-13 18:31:19 +0200

    Added admin labels to Profile

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "subscription", "subscribed_at")
+    list_filter = ("subscription", "subscribed_at")
+    search_fields = ("user__username", "user__email")
+    ordering = ("-subscribed_at",)
